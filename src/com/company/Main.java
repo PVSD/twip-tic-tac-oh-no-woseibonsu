b6983e8be718567c452019e8c651a4ca8d43b0f0
wo580 <winstonobonsu@gmail.com>
2018-12-17 11:21:46 -0500

    AHHHHH

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,26 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        String[][] Dan = new String[9][9];
+
+        for (int i = 1; i < 7; i++)
+        {
+            for (int i2 = 0; i2 < 9; i2++)
+            {
+                Dan[i][i2] = "▨";
+            }
+            i++;
+        }
+
+        for (int i = 1; i < 7; i++)
+        {
+            for (int i2 = 0; i2 < 9; i2++)
+            {
+                System.out.print(Dan[i][i2]);
+            }
+            System.out.println();
+            i++;
+        }
     }
 }
